839cebd44b00b52db218e73cb551ee31c6b52559
Anselem <anselemodims@gmail.com>
2022-04-19 08:29:19 +0100

    Feat: setup other actions on the categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,26 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(categories_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to categories_path, notice: 'Category was created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
