595f0466726c54391869c2ef78c283a457826788
Anselem <anselemodims@gmail.com>
2022-04-20 08:34:01 +0100

    Test: write tests for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :categories, foreign_key: 'author_id', dependent: :destroy
   has_many :deals, foreign_key: 'author_id', dependent: :destroy
+
+  validates :full_name, presence: true
+  validates :email, presence: true
 end
+
