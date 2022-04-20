12cdb027c672f277bdc88d0727bc7dde4d0c033c
Anselem <anselemodims@gmail.com>
2022-04-20 10:48:36 +0100

    Test: write tests for the deals index page

--- spec/integration/deals_index_spec.rb
+++ spec/integration/deals_index_spec.rb
@@ -13,12 +13,28 @@ RSpec.describe 'Categories index page', type: :feature do
   end
 
   it 'should have the following content' do
-    expect(page).to have_content('John')
-    expect(page).to have_content('Total Categories')
+    expect(page).to have_content('ALL TRANSACTIONS')
+    expect(page).to have_content('Total Transactions')
     expect(page).to have_link('Home')
     expect(page).to have_link('Transactions')
     expect(page).to have_link('Categories')
     expect(page).to have_link('Sign Out')
-    expect(page).to have_content('No categories added yet')
+    expect(page).to have_content('No transactions added for this category')
+  end
+
+  it 'should redirect to the right path' do
+    click_link 'Add New Transaction'
+    expect(current_path).to eq(new_deal_path)
+  end
+
+  it 'should display the created transaction' do
+    @category = @user.categories.create!(name: 'Food', icon: 'back_arrow1.png')
+    @transaction = @user.deals.create!(name: 'Rice Payment', amount: 100, category: @category)
+    visit '/deals'
+    expect(page).to have_content('ALL TRANSACTIONS')
+    expect(page).to have_content('Total Transactions')
+    expect(page).to have_content(@transaction.created_at)
+    expect(page).to have_content('$100')
+    expect(page).to have_link('Add New Transaction')
   end
 end
